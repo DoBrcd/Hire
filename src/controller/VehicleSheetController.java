e598c498c0a5fcdcb8777ecd43dd25567e1f7f6c
bozoin <bozoin@gmail.com>
2020-11-16 15:36:31 +0100

    Create VehicleSheetController.java

---
+++ src/controller/VehicleSheetController.java
@@ -0,0 +1,25 @@
+package controller;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Vehicle/Sheet")
+public class VehicleSheetController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head><title>Hello World VehicleSheetController</title></head>");
+		out.println("<body>");
+		out.println("<h1>Hello World VehicleSheetController !</h1>");
+		out.println("</body></html>");
+	}
+}
