5bba9abb936728e12f1cae8d94c83f11dcd3e5e4
Kévin PLESTAN <kplestan@enssat.fr>
2020-11-16 16:33:05 +0100

    Remove bad import

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -1,18 +1,16 @@
 package controller;
 
 
-import model.Vehicule;
 import service.DBManager;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/home")
 public class HomeController extends HttpServlet {
