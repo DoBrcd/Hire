050b53ff541b04e220aa8a526a8800def468b22f
ibrahim alnezami <ibrahimn12330@gmail.com>
2020-11-23 14:58:12 +0100

    supprimer lister vehicle

--- src/controller/ResearchVehicleController.java
+++ src/controller/ResearchVehicleController.java
@@ -1,26 +1,55 @@
 package controller;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Vehicle;
+import service.VehicleServiceImp;
+import service.VehicleServiceInterface;
+
 @WebServlet("/vehicle/research")
 public class ResearchVehicleController extends BaseController {
-	
-	@Override
+
+	private static final long serialVersionUID = 1L;
+
+	private final String pageName = "/vehicle/research.jsp";
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 		super.doGet(req, resp);
-		resp.setContentType("text/html");
-		PrintWriter out = resp.getWriter();
-		out.println("<html>");
-		out.println("<head><title>Hello World ResearchVehicleController</title></head>");
-		out.println("<body>");
-		out.println("<h1>Hello World ResearchVehicleController !</h1>");
-		out.println("</body></html>");
+		VehicleServiceInterface vService = new VehicleServiceImp();
+
+		List<Vehicle> data = vService.getAll();
+		req.setAttribute("data", data);
+		RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
+		try {
+			rd.forward(req, resp);
+		} catch (ServletException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 }
