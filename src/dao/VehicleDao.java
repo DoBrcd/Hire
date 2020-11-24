1607a961fbe591fca5072b7dbdc3ac235987279b
Wilfried PEPIN <wilfried.pepin35133@gmail.com>
2020-11-24 19:42:25 +0100

    coucou

--- src/dao/VehicleDao.java
+++ src/dao/VehicleDao.java
@@ -235,7 +235,6 @@ public class VehicleDao implements VehicleDaoInterface {
 	public List<Vehicle> getFreevehicle(String dateBegin, String dateEnd){
 		if (em != null) {
 			try {
-				System.out.println("coucou " + dateBegin);
 				String query = "SELECT DISTINCT v FROM Vehicle v LEFT JOIN v.hires h WHERE h IS NULL OR NOT (('"+ dateBegin + "' >= h.dateBegining AND '" + dateBegin + "' <= h.dateEnding) OR ('"+ dateEnd + "' >= h.dateBegining AND '"+ dateEnd +"' <= h.dateEnding))";
 				//String query = "SELECT v FROM Vehicle v WHERE v.id NOT IN( SELECT h.idVehicle_fk FROM Hire h WHERE ('"+ dateBegin + "' >= h.dateBegining AND '" + dateBegin + "' <= h.dateEnding) OR ('"+ dateEnd + "' >= h.dateBegining AND '"+ dateEnd +"' <= h.dateEnding))";
 				  List<Vehicle> vehicles = (List<Vehicle>) em.createQuery(query).getResultList();
